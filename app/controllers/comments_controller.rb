cae4b1c244901a0a10f7fd7b0ffb225a1d61f240
Jonathan Rutigliano <jonathanrutigliano@Jonathans-MacBook-Pro.local>
2023-11-06 15:30:00 -0800

    initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comments_params)
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
+end
